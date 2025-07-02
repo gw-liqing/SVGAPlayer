f01645444ba5e833140fdc4627deeefe9badb9ba
Max <>
2025-07-02 15:16:47 +0800

    feat:  编译优化

--- Source/pbobjc/Svga.pbobjc.m
+++ Source/pbobjc/Svga.pbobjc.m
@@ -13,6 +13,7 @@
 #import "GPBProtocolBuffers_RuntimeSupport.h"
 #endif
 
+@import Darwin.libkern.OSAtomic;
 #include <libkern/OSAtomic.h>
 #import "Svga.pbobjc.h"
 // @@protoc_insertion_point(imports)
